6777416a52d5deca1efe897b2f87c99e8ef9b012
AdrianFedeles <65623604+AdrianFedeles@users.noreply.github.com>
2021-03-30 21:09:25 +0300

    the second test

--- weatherapp/src/generalTemperature.tsx
+++ weatherapp/src/generalTemperature.tsx
@@ -6,7 +6,6 @@ class GeneralTemperature extends React.Component <any, any> {
     render() {
         return (
             <div className= "general">
-                <p>haw miau</p>
                 <p>This is a general temperature.</p>
                 <h1>Here is the weather twoday</h1>
             </div>
